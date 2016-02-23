45c3f556fc9eef5b6517ad978010cb55aeaf3619
Dan Cadden <shikkic@gmail.com>
2016-02-22 20:09:34 -0500

    added colors and cleaned up a bit

--- gitbar_plugin/gh.30s.js
+++ gitbar_plugin/gh.30s.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env /usr/local/bin/node
 
 /*
-bitbar metadata
 # <bitbar.title>GitHubBar</bitbar.title>
 # <bitbar.version>v1.0</bitbar.version>
-# <bitbar.author>Daniel Cadden</bitbar.author>
+# <bitbar.author>Dan Cadden</bitbar.author>
 # <bitbar.author.github>shikkic</bitbar.author.github>
 # <bitbar.desc>Quick check your Github stats</bitbar.desc>
 # <bitbar.image>http://www.hosted-somewhere/pluginimage</bitbar.image>
@@ -12,24 +11,42 @@ bitbar metadata
 # <bitbar.abouturl>https:/github.com/shikki</bitbar.abouturl>
 */
 
-var gh = require('gh-scrape'),
-    userUrl = "http://www.github.com/<YOUR USERNAME HERE>",
+// Define Constants
+const redText = "| color=red",
+    greenText = "| color=green",
+    userUrl = "http://www.github.com/shikkic",
     heartEmoji = String.fromCharCode(0xD83D, 0xDC9F),
-    brokenHeartEmoji = String.fromCharCode(0xD83D, 0xDC94),
+    brokenHeartEmoji = String.fromCharCode(0xD83D, 0xDC94);
+
+// Import Github Scraping Library
+var gh = require('gh-scrape'),
     visibleEmoji; 
 
-gh.scrapeContributionDataAndStats("http://www.github.com/shikkic", function(data) {
+// Scrape Github Stats for <userUrl> 
+gh.scrapeContributionDataAndStats("http://www.github.com/shikkic", function(data) { 
+
+    // Validate Request Data Exists
     if (data) {
+        // Retrive Request Data
         var commitsToday = data.commitsToday,
             currentStreak = data.statsData.currentStreak,
-            totalContributions = data.statsData.totalContributions,
-            visibleEmoji = data.commitsToday ? heartEmoji : brokenHeartEmoji;
+            totalContributions = data.statsData.totalContributions;
+        
+        // Set Text Color Variables 
+        var contributionsTodayColor = commitsToday ? greenText : redText,
+            currentStreakColor = currentStreak ? greenText : redText,
+            totalContributionsColor = totalContributions ? greenText : redText; 
 
-        console.log(visibleEmoji, "Contributions Today: ", commitsToday, visibleEmoji);
+        // Set Displayed Emoji
+        var visibleEmoji = data.commitsToday ? heartEmoji : brokenHeartEmoji;
+
+        // Log Output To Bitbar
+        console.log(visibleEmoji, "Contributions Today: ", commitsToday, visibleEmoji, contributionsTodayColor);
         console.log("---");
-        console.log("Current Streak: ", currentStreak + " days");
-        console.log("Total Contributions: ", totalContributions);
+        console.log("Current Streak: ", currentStreak, currentStreakColor);
+        console.log("Total Contributions: ", totalContributions, totalContributionsColor);
     } else {
-        console.log(brokenHeartEmoji + "error");
+        console.log(brokenHeartEmoji + "error" + brokenHeartEmoji);
     }
+
 });
